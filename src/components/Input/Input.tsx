0c8726d211db5f904575e74b939ddb3405ce5e86
brunnobird <bird.tec.gonzalez@gmail.com>
2022-07-21 12:22:08 -0300

    feat: primeira aula finalizada

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,9 +1,25 @@
-import { VStack } from 'native-base';
+import { Input as NativeBaseInput, IInputProps } from 'native-base';
 
-export function Input() {
+//...rest pega qualquer prop a mais que eu passar no component
+export function Input({ ...rest }: IInputProps) {
   return (
-    <VStack>
-
-    </VStack>
+    <NativeBaseInput
+      bg="gray.700"
+      h={14}
+      size="md"
+      borderWidth={0}
+      fontSize="md"
+      fontFamily="body"
+      color="white"
+      placeholderTextColor="gray.300"
+      selectionColor="white"
+      // _ = Pseudos propriedades
+      _focus={{
+        borderWidth: 1,
+        borderColor: "green.500",
+        bg: "gray.700"
+      }}
+      {...rest}
+    />
   );
 }
